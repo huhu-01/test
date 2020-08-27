ebdb722680edfaf9f82315eca9327454bc182d17
huhu-01 <64249077+huhu-01@users.noreply.github.com>
2020-08-27 17:08:39 +0800

    更改文件
    第一次更改

--- Java0827.java
+++ Java0827.java
@@ -1,5 +1,5 @@
 public class Java0827{
-				    public static void main(String args[]){
-	   	     System.out.println("Hello GitHub");
+	public static void main(String args[]){
+		System.out.println("Hello GitHub");
     }
 }
